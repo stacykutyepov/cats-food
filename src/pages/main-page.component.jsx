4cf5f667e7ee87175ab52d20c96fc8888c4a679d
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-26 13:58:15 -0400

    display Card with dimanic data

--- src/pages/main-page.component.jsx
+++ src/pages/main-page.component.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "./main-page.styles.scss";
-import Card from '../components/card/card-component';
+import Card from "../components/card/card-component";
+import { FOOD_DATA } from "../data/cats-food-data";
 
 const MainPage = () => {
   return (
     <div className="main-page">
-      <span className="page-title">Ты сегодня покормил кота?</span>
+      <h2 className="page-title">Ты сегодня покормил кота?</h2>
       <div className="cards-container">
-      <Card/>
-      <Card/>
-      <Card/>
-
-
+        {FOOD_DATA.map((item) => (
+          <Card
+            key={item.id}
+            topText={item.upperTitle}
+            title={item.title}
+            flavour={item.flavour}
+            portions={item.portions}
+            gift={item.gift}
+            satisfaction={item.satisfaction}
+            weight={item.weight}
+            availability={item.availability}
+            additionalInfo={item.additionalInfo}
+          />
+        ))}
       </div>
     </div>
   );
